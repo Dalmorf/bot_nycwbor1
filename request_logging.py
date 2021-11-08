d4a4309c6a1d8499dda1f1197b008ec225e93ed4
Dalmorf <burakomur@gmail.com>
2021-11-08 17:01:40 +0300

    commit3

--- request_logging.py
+++ request_logging.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 url = "https://www.nytimes.com/crosswords/game/mini"
 response = requests.get(url)
@@ -10,17 +9,18 @@ baslik = soup.find_all("h3",{"class":"ClueList-title--1-3oW"})
 sayi = soup.find_all("span",{"class":"Clue-label--2IdMY"})
 ipucu = soup.find_all("span",{"Clue-text--3lZl7"})
 
-liste = list()
 
 for i in range(len(baslik)):
     baslik[i] = (baslik[i].text).strip("\n").strip()
 
 for i in range(len(sayi)):
     sayi[i] = (sayi[i].text).strip("\n").strip()
-    ipucu[i] = (ipucu[i].text).strip("\n").strip()
-    liste.append([sayi[i],ipucu[i]])
+    ipucu[i] = (ipucu[i].text).strip("\n").strip()    
 
-
-df = pd.DataFrame(liste,columns=[baslik[0],baslik[1]])
-print(df)
+print("> === ",baslik[0]," ===")
+for i in range(0,5):
+    print("> ",sayi[i],". ",ipucu[i])
+print("> === ",baslik[1]," ===")   
+for i in range(5,10):
+    print("> ",sayi[i],". ",ipucu[i])
 
